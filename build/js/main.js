cf2e6496651a6d6f296dbd1f00d5f7442fed1967
Venera <coldlives@gmail.com>
2021-02-17 12:25:46 +0300

    добавляет валлидацию  проверки телефона

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,27 @@
 'use strict';
 
 (function () {
-  var
+  var inputTel = document.getElementById('tel-field');
+  var MIN_TITLE_LENGTH = 22;
 
+  var phoneMask = function () {
+    window.iMask.phone(inputTel, {
+      mask: '+{7} (000) 000 - 00 - 00',
+    });
+  };
 
+  phoneMask();
 
-});
+  var onInputTelInput = function () {
+    var numberLength = inputTel.value.length;
+
+    if (numberLength < MIN_TITLE_LENGTH) {
+      inputTel.setCustomValidity('Введите 10 цифр номера телефона.');
+    } else {
+      inputTel.setCustomValidity('');
+    }
+    inputTel.reportValidity();
+  };
+
+  inputTel.addEventListener('input', onInputTelInput);
+})();
